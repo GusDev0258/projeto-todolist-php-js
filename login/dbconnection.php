5e8797dbd685adadc1ebc56113eab4ee97dc3cbf
gusdev0258 <16326176760@edu.udesc.br>
2023-03-20 12:59:45 -0300

    add: gitignore

--- login/dbconnection.php
+++ login/dbconnection.php
@@ -1,9 +1,9 @@
 <?php
-$host = "";
-$port = "";
-$dbname = "";
-$user = "";
-$password = "";
+$host = "localhost";
+$port = "5432";
+$dbname = "localphptreinamento";
+$user = "postgres";
+$password = "PGCEAVI";
 
 $dbconnection = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password") or die("Failed to connect to DataBase");
 
